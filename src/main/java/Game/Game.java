73578d449d8d57cda09b45bda586bce555592b53
s1122126 <93487259+s1122126@users.noreply.github.com>
2022-04-06 13:52:18 +0200

    Update Game.java

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,16 +1,5 @@
 package Game;
 
-import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
-import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
-import static com.almasb.fxgl.dsl.FXGL.getGameController;
-import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
-import static com.almasb.fxgl.dsl.FXGL.onBtnDown;
-import static com.almasb.fxgl.dsl.FXGL.onCollisionBegin;
-import static com.almasb.fxgl.dsl.FXGL.onKey;
-import static com.almasb.fxgl.dsl.FXGL.run;
-import static com.almasb.fxgl.dsl.FXGL.showMessage;
-import static com.almasb.fxgl.dsl.FXGL.spawn;
-
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.entity.Entity;
@@ -18,6 +7,8 @@ import com.almasb.fxgl.entity.EntityFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.util.Duration;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class Game extends GameApplication {
 
@@ -37,6 +28,7 @@ public class Game extends GameApplication {
         settings.setHeight(750);
         settings.setWidth(1000);
         settings.setTitle("The Four Nerds and the Social Adventures of Terror");
+        settings.setAppIcon("");
     }
 
     @Override
@@ -63,6 +55,7 @@ public class Game extends GameApplication {
 
     @Override
     protected void initPhysics() {
+        getPhysicsWorld().setGravity(0,700);
         onCollisionBegin(EntityType.BULLET, EntityType.ENEMY, (bullet, enemy) -> {
             bullet.removeFromWorld();
             enemy.removeFromWorld();
