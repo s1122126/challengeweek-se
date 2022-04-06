62581c6310f4cc44ee5a34438a083adfab2b4332
s1122126 <93487259+s1122126@users.noreply.github.com>
2022-04-06 13:45:40 +0200

    okay

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -10,11 +10,9 @@ import static com.almasb.fxgl.dsl.FXGL.onKey;
 import static com.almasb.fxgl.dsl.FXGL.run;
 import static com.almasb.fxgl.dsl.FXGL.showMessage;
 import static com.almasb.fxgl.dsl.FXGL.spawn;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getAudioPlayer;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import javafx.scene.input.KeyCode;
@@ -28,6 +26,10 @@ public class Game extends GameApplication {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public enum EntityType {
+        PLAYER, BULLET, ENEMY
     }
 
     @Override
@@ -75,3 +77,92 @@ public class Game extends GameApplication {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import com.almasb.fxgl.app.GameApplication;
+//import com.almasb.fxgl.app.GameSettings;
+//import com.almasb.fxgl.dsl.FXGL;
+//import com.almasb.fxgl.entity.Entity;
+//import javafx.scene.input.KeyCode;
+//import javafx.scene.paint.Color;
+//import javafx.scene.shape.Rectangle;
+//
+//public class Game extends GameApplication {
+//
+//    private Entity player;
+//
+//    @Override
+//    protected void initSettings(GameSettings gameSettings) {
+//        gameSettings.setVersion("1.0");
+//        gameSettings.setTitle("The Four Nerds and the Social Adventures of Terror");
+//        gameSettings.setHeight(800);
+//        gameSettings.setWidth(800);
+//    }
+//
+//    @Override
+//    protected void initGame(){
+//        player = FXGL.entityBuilder()
+//                .at(200, 200)
+//                .view("mario.png")
+//                .buildAndAttach();
+//    }
+//
+//    @Override
+//    protected void initInput(){
+//        FXGL.onKey(KeyCode.W, () -> {
+//            player.translateY(-5);
+//        });
+//
+//        FXGL.onKey(KeyCode.A, () -> {
+//            player.translateX(-5);
+//        });
+//
+//        FXGL.onKey(KeyCode.S, () -> {
+//            player.translateY(5);
+//        });
+//
+//        FXGL.onKey(KeyCode.D, () -> {
+//            player.translateX(5);
+//        });
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
