07349d214f5530bc1482fac5907013a8665316a7
s1122126 <93487259+s1122126@users.noreply.github.com>
2022-04-06 13:38:30 +0200

    peop

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,7 +1,5 @@
 package Game;
 
-import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
-import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
 import static com.almasb.fxgl.dsl.FXGL.getGameController;
 import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
 import static com.almasb.fxgl.dsl.FXGL.onBtnDown;
@@ -10,18 +8,17 @@ import static com.almasb.fxgl.dsl.FXGL.onKey;
 import static com.almasb.fxgl.dsl.FXGL.run;
 import static com.almasb.fxgl.dsl.FXGL.showMessage;
 import static com.almasb.fxgl.dsl.FXGL.spawn;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getAudioPlayer;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.EntityFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.util.Duration;
 
 public class Game extends GameApplication {
+
+    private void getPhysicsWorld() {}
 
     private final PlayerFactory playerFactory = new PlayerFactory();
     private Entity player;
@@ -61,6 +58,9 @@ public class Game extends GameApplication {
 
     @Override
     protected void initPhysics() {
+        getPhysicsWorld().setGravity(0, 760);
+        getPhysicsWorld().addCollisionHandler(new PlayerButtonHandler());
+
         onCollisionBegin(EntityType.BULLET, EntityType.ENEMY, (bullet, enemy) -> {
             bullet.removeFromWorld();
             enemy.removeFromWorld();
@@ -72,6 +72,7 @@ public class Game extends GameApplication {
             });
         });
     }
+
 }
 
 
---
+++ src/main/java/Game/UI.java
@@ -0,0 +1,22 @@
+import com.almasb.fxgl.ui;
+import static com.almasb.fxgl.dsl.FXGL.getGameController;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGL.onBtnDown;
+import static com.almasb.fxgl.dsl.FXGL.onCollisionBegin;
+import static com.almasb.fxgl.dsl.FXGL.onKey;
+import static com.almasb.fxgl.dsl.FXGL.run;
+import static com.almasb.fxgl.dsl.FXGL.showMessage;
+import static com.almasb.fxgl.dsl.FXGL.spawn;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.util.Duration;
+
+package Game;
+
+public class UI {
+
+}
