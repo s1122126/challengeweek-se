2467e805f220aec7edb1da0f9e5b04b61489f68a
s1122126 <93487259+s1122126@users.noreply.github.com>
2022-04-07 14:11:44 +0200

    Changed zoom etc

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -36,8 +36,8 @@ public class Game extends GameApplication {
 
     @Override
     protected void initGame() {
-        Viewport viewport = getGameScene().getViewport();
 
+        Viewport viewport = getGameScene().getViewport();
         getGameWorld().addEntityFactory(this.playerFactory);
 //        setLevelFromMap("testlevel.tmx");
         setLevelFromMap("LevelSamurai.tmx");
@@ -52,7 +52,9 @@ public class Game extends GameApplication {
         this.player = spawn("player", 100, 1);
         this.enemy = spawn("enemy", 300, 1);
         // set view to player
-        viewport.bindToEntity(this.player, getAppWidth() / 2, getAppHeight() / 2);
+        viewport.setBounds(0,0,2000,1000);
+        viewport.setZoom(6.25);
+        viewport.bindToEntity(this.player, getAppWidth() / 2, (getAppHeight() / 2) + 300);
         viewport.setLazy(true);
         enemy.getComponent(EnemyController.class).onUpdate(1);
 
