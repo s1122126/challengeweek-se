0e1ebe244467786376239584fdcae998de25d011
enderluca03 <lucazon2003@gmail.com>
2022-04-07 10:32:04 +0200

    V4.3

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -2,26 +2,13 @@ package Game;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import static com.almasb.fxgl.dsl.FXGL.*;
-
 import com.almasb.fxgl.audio.Music;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.EntityFactory;
-import com.almasb.fxgl.entity.level.tiled.TiledMap;
-import javafx.scene.paint.CycleMethod;
 import enemy.Ninja;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.util.Duration;
-import javafx.animation.Animation;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class Game extends GameApplication {
 
@@ -62,7 +49,6 @@ public class Game extends GameApplication {
         this.ninja = spawn("enemy", ninja1.getSpawnX(), ninja1.getSpawnY());
     }
 
-
     @Override
     protected void initInput() {
         onKey(KeyCode.W, () -> player.translateY(-5));
@@ -88,55 +74,3 @@ public class Game extends GameApplication {
         });
     }
 }
-
-//import com.almasb.fxgl.app.GameApplication;
-//import com.almasb.fxgl.app.GameSettings;
-//import com.almasb.fxgl.dsl.FXGL;
-//import com.almasb.fxgl.entity.Entity;
-//import javafx.scene.input.KeyCode;
-//import javafx.scene.paint.Color;
-//import javafx.scene.shape.Rectangle;
-//
-//public class Game extends GameApplication {
-//
-//    private Entity player;
-//
-//    @Override
-//    protected void initSettings(GameSettings gameSettings) {
-//        gameSettings.setVersion("1.0");
-//        gameSettings.setTitle("The Four Nerds and the Social Adventures of Terror");
-//        gameSettings.setHeight(800);
-//        gameSettings.setWidth(800);
-//    }
-//
-//    @Override
-//    protected void initGame(){
-//        player = FXGL.entityBuilder()
-//                .at(200, 200)
-//                .view("mario.png")
-//                .buildAndAttach();
-//    }
-//
-//    @Override
-//    protected void initInput(){
-//        FXGL.onKey(KeyCode.W, () -> {
-//            player.translateY(-5);
-//        });
-//
-//        FXGL.onKey(KeyCode.A, () -> {
-//            player.translateX(-5);
-//        });
-//
-//        FXGL.onKey(KeyCode.S, () -> {
-//            player.translateY(5);
-//        });
-//
-//        FXGL.onKey(KeyCode.D, () -> {
-//            player.translateX(5);
-//        });
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
