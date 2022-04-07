c4b645fbcc33d9ea6ee2470eb36a3a8997751f78
Mickerduud <39124471+Mickerduud@users.noreply.github.com>
2022-04-07 10:04:08 +0200

    img and tsx files

---
+++ target/classes/Assets/levels/NES - Rygar Argos no Senshi - 01 - Argool - Sueru Mountain.tsx
@@ -0,0 +1,3 @@
+<tileset version="1.8" tiledversion="1.8.4" name="NES - Rygar Argos no Senshi - 01 - Argool - Sueru Mountain" tilewidth="32" tileheight="32" tilecount="456" columns="57">
+ <image source="NES - Rygar Argos no Senshi - 01 - Argool - Sueru Mountain.png" width="1824" height="272"/>
+</tileset>
