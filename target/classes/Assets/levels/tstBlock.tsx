96cf6b3ac42086c0199be4cce749b4bf2851d707
enderluca03 <lucazon2003@gmail.com>
2022-04-08 10:33:53 +0200

    v5.6

--- target/classes/Assets/levels/tstBlock.tsx
+++
@@ -1,3 +0,0 @@
-<tileset version="1.8" tiledversion="1.8.4" name="tstBlock" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="tstBlock.png" width="173" height="173"/>
-</tileset>
