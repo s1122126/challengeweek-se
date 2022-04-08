332a7031e44a3d082425624757c7e0445adcf8a2
enderluca03 <lucazon2003@gmail.com>
2022-04-08 09:04:51 +0200

    v5.0

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -43,8 +43,7 @@ public class Game extends GameApplication {
 
         getGameWorld().addEntityFactory(this.playerFactory);
 //        setLevelFromMap("testlevel.tmx");
-        setLevelFromMap("LevelSamurai.tmx");
-
+        setLevelFromMap("Level1.tmx");
 
         //music
         Music music = getAssetLoader().loadMusic("Music.wav");
