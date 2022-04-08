d56b7d16c7452124c9b925c577f20c4b1761bdb8
Mickerduud <39124471+Mickerduud@users.noreply.github.com>
2022-04-08 10:25:33 +0200

    Collidable walls

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -54,8 +54,8 @@ public class Game extends GameApplication {
         this.player = spawn("player", 100, 1);
         this.enemy = spawn("enemy", 300, 1);
         // set view to player
-//        viewport.setBounds(0,0,2000,1000);
-//        viewport.setZoom(6.25);
+        viewport.setBounds(0,0,2000,1000);
+        viewport.setZoom(6.25);
         viewport.bindToEntity(this.player,  getAppWidth() / 2, (getAppHeight() / 2) + 300);
 
         viewport.setBounds(0,0,2000,1000);
